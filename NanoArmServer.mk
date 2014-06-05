bfd37f21725104dc9d7f9765b8ecf7e2d22fd2b5
Ma Eikei <maeikei@gmail.com>
2014-06-05 23:15:29 +0900

    new

--- NanoArmServer.mk
+++ NanoArmServer.mk
@@ -1,4 +1,4 @@
 all:
 	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- NanoArmServer_defconfig
-	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage NanoArmServer.dtb
+	make -j4 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage NanoArmServer.dtb
 	cat arch/arm/boot/zImage arch/arm/boot/dts/NanoArmServer.dtb > zImage-dtb
